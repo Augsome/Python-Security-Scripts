2dff82a252192d676f1b07a81efe368270fc3035
Augsome <92285755+Augsome@users.noreply.github.com>
2023-09-11 15:49:11 -0500

    Create hash crack

---
+++ hash-crack.py
@@ -0,0 +1,24 @@
+# Hash Cracker Script
+
+'''
+Script explanation
+
+1. Ask for the location of the wordlist
+2. Ask for the MD5 hash to be cracked
+3. Reads values from wordlist
+4. Converts cleartext values to MD5 hashes
+5. Compares the generated MD5 hash value with the value entered by the user
+'''
+
+import hashlib
+
+wordlist_location = str(input('Enter wordlist file location: '))
+hash_input = str(input('Enter hash to be cracked: '))
+
+with open(wordlist_location, 'r') as file:
+	for line in file.readlines()
+	hash_ob = hashlib.md5(line.strip().encode())
+	hashed_pass = hash_ob.hexdigest()
+	if hashed_pass == hash_input:
+		print('Found cleartext password! ' + line.strip())
+		exit(0)
