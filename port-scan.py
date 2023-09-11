48ddd8572708b43957b59792805d6aff3cb5be8d
Augsome <92285755+Augsome@users.noreply.github.com>
2023-09-11 15:06:00 -0500

    update port scan
    Added comments

--- port-scan.py
+++ port-scan.py
@@ -5,18 +5,27 @@ Script aiming to build a simple port scanner.
 Also, added an ascii banner cause why not??
 '''
 
+#Importing Modules for code
 import sys
 import socket
 import pyfiglet
 
+#ascii banner cause why not??
 ascii_banner = pyfiglet.figlet_format("Augsome's \n Python \n Port Scanner!")
 print(ascii_banner)
 
+#Target machine
 ip = '192.168.1.6'
+#ip = socket.gethostbyname(host) #Example of obtaining IP from domain name target.
+
+#Empty ports array to be populated with detected ports
 open_ports = []
 
-ports = range(1, 65535)
+#Ports to be probed
+ports = range(1, 65535) #Complete range of ports
+#ports = {21,22,23,53,80,135,443,445} #Example of probing specific ports
 
+#Function attempting to connect to ports
 def probe_port(ip, port, result = 1):
 	try:
 		sock - socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,12 +38,14 @@ def probe_port(ip, port, result = 1):
 		pass
 	return result
 
+#Loop to iterate through the specified port list
 for port in ports:
 	sys.stdout.flush()
 	response = probe_port(ip, port)
 	if response == 0:
 		open_ports.append(port)
 
+#Outcome of the port probing 
 if open_ports:
 	print("The open ports are: ")
 	print(sorted(open_ports))
