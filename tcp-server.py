f7bded8968183b6faca7cc7a70f95439da50f4ac
Augsome <92285755+Augsome@users.noreply.github.com>
2023-09-25 22:39:53 -0500

    Update tcp-server
    Added Comments.

--- tcp-server.py
+++ tcp-server.py
@@ -17,7 +17,7 @@ def main():
                 client, address = server.accept()
                 print(f'[*] Accepted connection from {address[0]}:{address[1]}')
                 client_handler = threading.Thread(target=handle_client, args=(client>
-                client_handler.start() # Start thread to handle client connection
+                client_handler.start() # Start thread to handle the client connection
 
 def handle_client(client_socket):
         with client_socket as sock:
