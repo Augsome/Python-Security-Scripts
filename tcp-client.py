f25a397e567d2d0dfc7590b0a120f8fa82d7a055
Augsome <92285755+Augsome@users.noreply.github.com>
2023-09-26 17:21:32 -0500

    Update tcp-client.py
    Continued refactoring

--- tcp-client.py
+++ tcp-client.py
@@ -16,10 +16,8 @@ client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 # Receive some data
 response = client.recv(4096)
 
-print(response.decode())
 client.close()
-
-# Code makes
+print(response)
 
 '''
 Code snippet makes some serious assumptions about sockets.
@@ -27,6 +25,6 @@ Code snippet makes some serious assumptions about sockets.
 1st assumes that our connection will always succeed.
 2nd is that the server expects us to send data first (sometimes the server sends first and awaits a client response.)
 3rd is that the server will always return data in a timely fashion.
- '''
+'''
 
 
